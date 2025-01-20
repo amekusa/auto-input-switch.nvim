a92bcaff8e9013cdcc0dd679a1d79412a6069140
Satoshi Soma <noreply@amekusa.com>
2025-01-20 23:22:28 +0900

    option descriptions

--- lua/auto-input-switch/init.lua
+++ lua/auto-input-switch/init.lua
@@ -57,10 +57,10 @@ local exec_get = vim.fn.system
 local M = {}
 function M.setup(opts)
 	local defaults = {
-		activate = true,
+		activate = true, -- Activate the plugin? (You can toggle this with `AutoInputSwitch on|off` command at any time)
 		normalize = {
-			enable = true,
-			on = {
+			enable = true, -- Enable to normalize the input-source?
+			on = { -- When to normalize (:h events)
 				'InsertLeave',
 				'BufLeave',
 				'WinLeave',
@@ -69,21 +69,29 @@ function M.setup(opts)
 			},
 		},
 		restore = {
-			enable = true,
-			on = {
+			enable = true, -- Enable to restore the input-source?
+			on = { -- When to restore (:h events)
 				'InsertEnter',
 				'FocusGained',
 			},
-			file_pattern = nil,
+			file_pattern = nil, -- File pattern to enable it on (nil to any file)
+			-- Example:
+			-- file_pattern = { '*.md', '*.txt' },
+
 			exclude_pattern = '[-a-zA-Z0-9=~+/?!@#$%%^&_(){}%[%];:<>]',
+			-- When you switch to insert-mode, the plugin checks the cursor position at the moment.
+			-- And if any of the characters before & after the position match with `exclude_pattern`,
+			-- the plugin cancel to restore the input-source and leave it as it is.
+			-- The default value of `exclude_pattern` is alphanumeric characters with a few exceptions.
 		},
-		os = nil, -- macos/windows/linux or nil to auto-detect
-		os_settings = {
+		os = nil, -- 'macos', 'windows', 'linux', or nil to auto-detect
+		os_settings = { -- OS-specific settings
 			macos = {
 				enable = true,
-				cmd_get = 'im-select', -- command to get the current input-source
-				cmd_set = 'im-select %s', -- command to set the input-source (use `%s` as a placeholder)
-				normal_input = nil, -- auto
+				cmd_get = 'im-select', -- Command to get the current input-source
+				cmd_set = 'im-select %s', -- Command to set the input-source (Use `%s` as a placeholder for the input-source)
+				normal_input = nil, -- Name of the input-source to normalize to when you leave insert-mode (Set nil to auto-detect)
+				-- Examples:
 				-- normal_input = 'com.apple.keylayout.ABC',
 				-- normal_input = 'com.apple.keylayout.US',
 				-- normal_input = 'com.apple.keylayout.USExtended',
