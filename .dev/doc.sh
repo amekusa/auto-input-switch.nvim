5876050d6178ede2c5eb0ce3d7f8de3151b700d3
Satoshi Soma <noreply@amekusa.com>
2025-10-19 19:27:06 +0900

    [dev] doc.sh: improve

--- .dev/doc.sh
+++ .dev/doc.sh
@@ -2,13 +2,21 @@
 
 base="$(dirname "$0")"
 
+# generate defaults.lua and the options doc
+node "$base/options.js"
+
+# generate the main doc
 cd "$base/panvimdoc"
 prj="auto-input-switch.nvim"
 
 ./panvimdoc.sh \
-	--project-name "$prj" \
 	--input-file "../../README.md" \
-	--shift-heading-level-by -1
+	--project-name "$prj" \
+	--shift-heading-level-by -1 \
+	--demojify true \
 
 cp -f "doc/$prj.txt" "../../doc/$prj.txt"
+
+# force neovim to reindex the doc
+nvim --headless -c "helptags ../../doc" -c "q"
 
