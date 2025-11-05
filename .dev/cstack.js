6dd6f36aa19193f63901f55a9ce92827e5e8ff77
Satoshi Soma <noreply@amekusa.com>
2025-11-05 11:31:43 +0900

    [dev] cstack: improve

--- .dev/cstack.js
+++ .dev/cstack.js
@@ -7,8 +7,18 @@ class ContextStack {
 		this.stack = [];
 	}
 
-	push(type, c) {
-		c.__type = type;
+	push(atts, c, next = null) {
+		let {id, classes} = parseAtts(atts);
+		if (next) {
+			atts = {classes};
+			let anc = this.get(atts); // ancestor
+			if (anc) {
+				anc = structuredClone(anc);
+				return this.push(atts, next(anc) || anc);
+			}
+		}
+		if (id) c._id = id;
+		if (classes) c._classes = classes;
 		this.stack.push(c);
 		return c
 	}
@@ -17,18 +27,37 @@ class ContextStack {
 		return this.stack.pop();
 	}
 
-	get(type, first = null, next = null) {
+	get(atts, fb = null) {
+		let {id, classes} = parseAtts(atts);
+		let found;
+		find_c:
 		for (let i = this.stack.length - 1; i >= 0; i--) {
-			if (this.stack[i].__type === type) {
-				if (next) {
-					let c = structuredClone(this.stack[i]);
-					return this.push(type, next(c) || c);
+			let c = this.stack[i];
+			if (id && id != c._id) continue;
+			if (classes) {
+				if (!c._classes) continue;
+				for (let i = 0; i < classes.length; i++) {
+					if (!c._classes.includes(classes[i])) continue find_c;
 				}
-				return this.stack[i];
 			}
+			found = c;
+			break;
 		}
-		return first ? this.push(type, first) : null;
+		return found || fb;
 	}
+}
+
+function parseAtts(atts) {
+	if (typeof atts == 'object') return atts;
+	let r = {};
+	atts = atts.split('.');
+	if (!atts[0]) atts.shift();
+	else if (atts[0].startsWith('#')) r.id = atts.shift().substring(1);
+	if (atts.length > 0) {
+		r.classes = [];
+		for (let i = 0; i < atts.length; i++) r.classes.push(atts[i]);
+	}
+	return r;
 }
 
 export default ContextStack;
